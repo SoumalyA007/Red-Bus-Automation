1a510f596909626baf4c40430ee4c3bbe757d57b
SoumalyA007 <soumalya.uchalan@gmail.com>
2026-07-20 03:37:36 +0530

    changes

--- src/test/java/testCases/AutoSuggestionTest.java
+++ src/test/java/testCases/AutoSuggestionTest.java
@@ -1,23 +1,34 @@
 package testCases;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import testBase.BaseClass;
 
-import java.util.List;
+import testBase.BaseClass;
 
 public class AutoSuggestionTest extends BaseClass {
 
     @Test
-    public void TC_001_suggestion_appears(){
+    public void TC_001_suggestion_appears() {
 
         String testName = "TC_001_suggestion_appears";
-        try{
+        int[] previousSize = {-1}; // array to hold the previous size of suggestions
+        try {
+            FluentWait<WebDriver> fluentWait = new FluentWait<>(driver)
+                    .withTimeout(Duration.ofSeconds(10))
+                    .pollingEvery(Duration.ofMillis(500))
+                    .ignoring(StaleElementReferenceException.class);
             //clicked on the source box
+
             hp.clickJourneyFrom();
             //need to wait for suggestion box to appear
             hp.isSuggestionsVisible();
@@ -28,21 +39,73 @@ public class AutoSuggestionTest extends BaseClass {
             currentFocusedSourceElement.sendKeys(Keys.DELETE);
             //need to send the source now
             currentFocusedSourceElement.sendKeys("Mumbai");
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class,'searchCategory___')]")));
-            List<WebElement> suggestionElements = driver.findElements(By.xpath("//div[contains(@class,'searchCategory___')]"));
+
+            wait.until(ExpectedConditions.presenceOfElementLocated(
+                    By.xpath("//div[contains(@class,'searchCategory___')][not(@role='region')]")
+            ));
+
+            List<WebElement> suggestionElements = driver.findElements(
+                    By.xpath("//div[contains(@class,'searchCategory___')]"));
+
             Assert.assertEquals(suggestionElements.size(), 3);
 
         } catch (Exception e) {
             logTestFailure(testName, e);
         }
 
+    }
 
+    @Test
+    public void TC_002_suggestion_disappears_after_selection() {
+
+        String testName = "TC_002_suggestion_disappears_after_selection";
+        try {
+
+            helper.enter_source("Mumbai");
+            helper.enter_destination("Pune");
+
+            List<WebElement> suggestions = driver.findElements(
+                    By.xpath("//div[contains(@class,'searchSuggestionWrapper___')]")
+            );
+
+            Assert.assertTrue(suggestions.isEmpty(), "Suggestions are still visible.");
+            logTestPass(testName);
+        } catch (Exception e) {
+            logTestFailure(testName, e);
+        }
 
     }
 
+    @Test
+    public void TC_OO3_Keyboard_navigation() {
+        String testName = "TC_OO3_Keyboard_navigation";
+        try {
+            hp.clickJourneyFrom();
+            //need to wait for suggestion box to appear
+            hp.isSuggestionsVisible();
+            //after waiting for suggestion box to appear we need to move the focus to the current element
+            WebElement currentFocusedSourceElement = driver.switchTo().activeElement();
+            //need to send the source now
+            currentFocusedSourceElement.sendKeys("Mumbai");
+            currentFocusedSourceElement.sendKeys(Keys.ARROW_DOWN);
+            currentFocusedSourceElement.sendKeys(Keys.ENTER);
 
+            hp.clickJourneyTo();
+            //need to wait for suggestion box to appear
+            hp.isSuggestionsVisible();
+            //after waiting for suggestion box to appear we need to move the focus to the current element
+            WebElement currentFocusedDestElement = driver.switchTo().activeElement();
+            //send the required destination
+            currentFocusedDestElement.sendKeys("Kolkata");
+            currentFocusedSourceElement.sendKeys(Keys.ARROW_DOWN);
+            currentFocusedSourceElement.sendKeys(Keys.ENTER);
 
+            Assert.assertEquals(hp.getCurrentSource(), "Mumbai");
+            Assert.assertEquals(hp.getCurrentDestination(), "Kolkata");
 
+        } catch (Exception e) {
+            logTestFailure(testName, e);
+        }
+    }
 
 }
-
